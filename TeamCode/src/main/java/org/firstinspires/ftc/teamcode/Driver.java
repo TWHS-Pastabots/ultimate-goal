d17c8eaa1a5d53a02be8c6609146db2850096b38
Xevion <xevioni@yandex.com>
2020-10-06 15:18:53 -0500

    Clean up Driver telop code, switch from local duplicate variables to direct instance variable access

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driver.java
@@ -7,12 +7,12 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Driver", group="Linear OpMode")
-public class Driver extends OpMode{
+@TeleOp(name = "Driver", group = "Linear OpMode")
+public class Driver extends OpMode {
 
     /* Declare OpMode members. */
     RobotHardware robot = new RobotHardware();
-    ElapsedTime runTime= new ElapsedTime();
+    ElapsedTime runTime = new ElapsedTime();
     ElapsedTime backTimer2 = new ElapsedTime();
     ElapsedTime intakeDelayTimer = new ElapsedTime();
     boolean isBlockPush = false;
@@ -52,91 +52,58 @@ public class Driver extends OpMode{
     public void start() {
         runTime.reset();
         telemetry.addData("Run Time", "reset");
-        closedSoundEffect = hardwareMap.appContext.getResources().getIdentifier("closediggy",
-                "raw", hardwareMap.appContext.getPackageName());
-        openSoundEffect = hardwareMap.appContext.getResources().getIdentifier("openiggy",
-                "raw", hardwareMap.appContext.getPackageName());
+//        closedSoundEffect = hardwareMap.appContext.getResources().getIdentifier("closediggy",
+//                "raw", hardwareMap.appContext.getPackageName());
+//        openSoundEffect = hardwareMap.appContext.getResources().getIdentifier("openiggy",
+//                "raw", hardwareMap.appContext.getPackageName());
         SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, openSoundEffect);
-
     }
 
-    // loop on start()
+    // Loop on start()
     @Override
     public void loop() {
-        double G1leftStickX = gamepad1.left_stick_x; //Changed to negative
-        double G1leftStickY = -gamepad1.left_stick_y;
-        double turnCon = gamepad1.right_stick_x*.75;
-        boolean G1a = gamepad1.a;
-        boolean G1b = gamepad1.b;
-        boolean G2a = gamepad2.a;
-        boolean G2b = gamepad2.b;
-        boolean G2x = gamepad2.x;
-        boolean G2y = gamepad2.y;
-        boolean G1y = gamepad1.y;
-        double G2leftStickY = -gamepad2.left_stick_y;
-        boolean G2rb = gamepad2.right_bumper;
-        boolean G2lb = gamepad2.left_bumper;
-        boolean G1down = gamepad1.dpad_down;
-        boolean G1up = gamepad1.dpad_up;
-        boolean G2up = gamepad2.dpad_up;
-        boolean G2down = gamepad2.dpad_down;
-        boolean G2left = gamepad2.dpad_left;
-        boolean G2right = gamepad2.dpad_right;
-        // example how to use buttons
-        double radius = Math.hypot( G1leftStickX, G1leftStickY);
-        double ang = Math.atan2( G1leftStickY, G1leftStickX) - Math.PI/4;
+        double radius = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
+        double ang = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+
+        double turnCon = gamepad1.right_stick_x * .75;
         double v1 = radius * Math.cos(ang) + turnCon;
         double v2 = radius * Math.sin(ang) - turnCon;
         double v3 = radius * Math.sin(ang) + turnCon;
         double v4 = radius * Math.cos(ang) - turnCon;
+
         // Sets power of motor, spins wheels
+        if (gamepad1.b) slowCon = .4;
+        if (gamepad1.a) slowCon = .8;
+        if (gamepad1.x) slowCon = 1.0;
 
-        if (G1b){
-            slowCon = .4;
-
-        }
-        if (G1a){
-            slowCon = .8;
-        }
-        if (gamepad1.x){
-            slowCon = 1.0;
-        }
-
-        // intake code
-        if(gamepad1.right_bumper){
-            if(isBlockPush){
+        // Intake code
+        if (gamepad1.right_bumper) {
+            if (isBlockPush) {
                 isBlockPush = false;
                 intakeDelayTimer.reset();
             }
-            if(intakeDelayTimer.seconds() > 1.0){
+            if (intakeDelayTimer.seconds() > 1.0) {
                 robot.leftIn.setPower(-.7);
                 robot.rightIn.setPower(-.7);
             }
-        }
-        else if(gamepad1.left_bumper){
+        } else if (gamepad1.left_bumper) {
             robot.leftIn.setPower(.7);
             robot.rightIn.setPower(.7);
-        }
-        else {
+        } else {
             robot.leftIn.setPower(0);
             robot.rightIn.setPower(0);
         }
 
         // lift code
-        if (G2up)
-        {
+        if (gamepad2.dpad_up) {
             robot.liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             robot.liftMotor.setPower(-1);
             pos = robot.liftMotor.getCurrentPosition();
-        }
-        else if (G2down)
-        {
+        } else if (gamepad2.dpad_down) {
             robot.liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             robot.liftMotor.setPower(0.4);
             pos = robot.liftMotor.getCurrentPosition();
-        }
-        else
-        {
+        } else {
             robot.liftMotor.setPower(0.0);
             robot.liftMotor.setTargetPosition(pos);
             robot.liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -145,70 +112,61 @@ public class Driver extends OpMode{
             telemetry.update();
         }
 
-        //servos
-        if (gamepad1.right_trigger > 0.9)
-        {
+        // Servos
+        if (gamepad1.right_trigger > 0.9) {
             robot.leftH.setPosition(0.75);
             robot.rightH.setPosition(0.9);
-        }
-        else if (gamepad1.left_trigger > 0.9)
-        {
+        } else if (gamepad1.left_trigger > 0.9) {
             robot.leftH.setPosition(0);
             robot.rightH.setPosition(0);
         }
 
-        if(gamepad2.back && backTimer2.seconds() > 1.0){
+        if (gamepad2.back && backTimer2.seconds() > 1.0) {
             isBlockPush = !isBlockPush;
-            if(isBlockPush)
+            if (isBlockPush)
                 SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, closedSoundEffect);
             else
                 SoundPlayer.getInstance().startPlaying(hardwareMap.appContext, openSoundEffect);
             backTimer2.reset();
         }
 
-        // claw
-        if (G2a) {
+        // Claw
+        if (gamepad2.a) {
             robot.claw.setPosition(1);
-        }
-        else if (G2b) {
+        } else if (gamepad2.b) {
             robot.claw.setPosition(0.5);
         }
 
-        if(G2right) {
+        if (gamepad2.dpad_right) {
             robot.clawT.setPosition(0);
-        }
-        else if (G2left) {
+        } else if (gamepad2.dpad_left) {
             robot.clawT.setPosition(1);
         }
 
-        if(gamepad2.right_trigger > 0.1){
+        if (gamepad2.right_trigger > 0.1) {
             robot.capstone.setDirection(CRServo.Direction.REVERSE);
             robot.capstone.setPower(1.0);
-        }
-        else if (gamepad2.left_trigger > 0.1){
+        } else if (gamepad2.left_trigger > 0.1) {
             robot.capstone.setDirection(CRServo.Direction.FORWARD);
             robot.capstone.setPower(1.0);
-        }
-        else{
+        } else {
             robot.capstone.setPower(0.0);
         }
 
 
-        if(gamepad1.dpad_up){
+        if (gamepad1.dpad_up) {
             robot.tape.setPower(-1.0);
-        }
-        else if(gamepad1.dpad_down){
+        } else if (gamepad1.dpad_down) {
             robot.tape.setPower(1.0);
-        }
-        else
+        } else
             robot.tape.setPower(0.0);
 
-        robot.leftFront.setPower(v1*slowCon );
-        robot.rightFront.setPower(v2*slowCon );
-        robot.leftRear.setPower(v3*slowCon );
-        robot.rightRear.setPower(v4*slowCon );
+        robot.leftFront.setPower(v1 * slowCon);
+        robot.rightFront.setPower(v2 * slowCon);
+        robot.leftRear.setPower(v3 * slowCon);
+        robot.rightRear.setPower(v4 * slowCon);
 
-        if(isBlockPush)
+        if (isBlockPush)
             robot.servo_blockPush.setPosition(0.0);
         else
             robot.servo_blockPush.setPosition(1.0);
@@ -225,7 +183,7 @@ public class Driver extends OpMode{
 
     // run once on stop()
     @Override
-    public void stop(){
+    public void stop() {
     }
 
 }
