0cb70a7103843309a176e6f62003b49a6402b8cd
Xevion <xevioni@yandex.com>
2021-02-09 14:05:40 -0600

    update MacaroniHardware to use RobotHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MacaroniHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MacaroniHardware.java
@@ -1,28 +1,19 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import static org.firstinspires.ftc.teamcode.hardware.ComponentIds.*;
+import java.util.Arrays;
 
 /**
  * Formal class for organizing all RobotHardware for usage across different Telop/Autonomous driving modes.
  * Contains references to HardwareMap and all motors, encoders and sensors used.
  */
-public class MacaroniHardware {
-    // Primary wheel motors
-    public DcMotor leftFrontMotor = null;
-    public DcMotor leftRearMotor = null;
-    public DcMotor rightFrontMotor = null;
-    public DcMotor rightRearMotor = null;
-
-    public DcMotor[] motors;
-    public DcMotor[] wheels;
-
-    public DcMotor intakeMotor;
-    public DcMotor beltMotor;
-    public DcMotor launcherMotor;
+public class MacaroniHardware extends RobotHardware {
+    public DcMotorEx intakeMotor;
+    public DcMotorEx beltMotor;
+    public DcMotorEx launcherMotor;
 
     public Servo clawServo;
     public Servo armServo;
@@ -34,40 +25,21 @@ public class MacaroniHardware {
         // Save reference to Hardware map
         this.hwMap = hwMap;
 
-        leftFrontMotor = hwMap.get(DcMotor.class, LEFT_FRONT_MOTOR);
-        rightFrontMotor = hwMap.get(DcMotor.class, RIGHT_FRONT_MOTOR);
-        leftRearMotor = hwMap.get(DcMotor.class, LEFT_REAR_MOTOR);
-        rightRearMotor = hwMap.get(DcMotor.class, RIGHT_REAR_MOTOR);
-
-        intakeMotor = hwMap.get(DcMotor.class, MacaroniIds.INTAKE_MOTOR);
-        launcherMotor = hwMap.get(DcMotor.class, MacaroniIds.LAUNCHER_MOTOR);
-        beltMotor = hwMap.get(DcMotor.class, MacaroniIds.BELT_MOTOR);
+        intakeMotor = hwMap.get(DcMotorEx.class, MacaroniIds.INTAKE_MOTOR);
+        launcherMotor = hwMap.get(DcMotorEx.class, MacaroniIds.LAUNCHER_MOTOR);
+        beltMotor = hwMap.get(DcMotorEx.class, MacaroniIds.BELT_MOTOR);
 
         armServo = hwMap.get(Servo.class, MacaroniIds.LOWER_WOBBLE_SERVO);
         clawServo = hwMap.get(Servo.class, MacaroniIds.UPPER_WOBBLE_SERVO);
 
-        wheels = new DcMotor[]{leftFrontMotor, rightFrontMotor, leftRearMotor, rightRearMotor};
-        motors = new DcMotor[]{leftFrontMotor, rightFrontMotor, leftRearMotor, rightRearMotor, intakeMotor, launcherMotor, beltMotor};
+        motors.addAll(Arrays.asList(intakeMotor, launcherMotor, beltMotor));
+        servos.addAll(Arrays.asList(armServo, clawServo));
 
-        // Motor direction is FORWARD by default
-        leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
-        leftRearMotor.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
-        rightRearMotor.setDirection(DcMotor.Direction.REVERSE);
+        motorRightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        motorRightRear.setDirection(DcMotorEx.Direction.REVERSE);
+        beltMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        intakeMotor.setDirection(DcMotorEx.Direction.REVERSE);
 
-        // These are built reversed
-        beltMotor.setDirection(DcMotor.Direction.REVERSE);
-        intakeMotor.setDirection(DcMotor.Direction.REVERSE);
-
-        // Setup motors
-        for (DcMotor motor : motors) {
-            // Set all motors to zero power
-            motor.setPower(0);
-            // Motors will break on Zero power
-            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            // Set all motors to run without encoders.
-            // May want to use RUN_USING_ENCODERS if encoders are installed.
-            motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
+        initializeComponents();
     }
 }
