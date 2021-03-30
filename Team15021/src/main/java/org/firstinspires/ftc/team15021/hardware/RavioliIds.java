8127f06ae2bc55ae93930b818a96f8bb088f1d4a
Matt Provost <mattprovost6@gmail.com>
2021-03-30 10:30:48 -0500

    Cleanup 15021

---
+++ Team15021/src/main/java/org/firstinspires/ftc/team15021/hardware/RavioliIds.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.team15021.hardware;
+
+public class RavioliIds {
+    public static final String MOTOR_CONVEYOR = "conveyor";
+    public static final String MOTOR_LAUNCHER = "launcher";
+    public static final String MOTOR_INTAKE = "intake";
+    public static final String MOTOR_CLAW = "motorClaw";
+    public static final String SERVO_CLAW = "servo_claw";
+    public static final String INTAKE_LOCK = "intakeLock";
+}
