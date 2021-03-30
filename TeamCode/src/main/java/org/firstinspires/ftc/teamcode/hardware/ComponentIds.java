9c651e7acf4a9e7216d50dc7f1ce7b750db25779
Matt Provost <mattprovost6@gmail.com>
2021-03-30 10:58:36 -0500

    Cleanup component ids

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/ComponentIds.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/ComponentIds.java
@@ -13,29 +13,9 @@ public class ComponentIds {
     public static final String RIGHT_REAR_MOTOR = "rightRear";
 
     // Encoders
-    public static final String LEFT_ENCODER = "launcherMotor";
-    public static final String RIGHT_ENCODER = "intakeMotor";
-    public static final String FRONT_ENCODER = "beltMotor";
+    public static final String LEFT_ENCODER = "leftFront";
+    public static final String RIGHT_ENCODER = "rightFront";
+    public static final String FRONT_ENCODER = "leftRear";
 
     public static final String IMU = "imu";  // Used by Road Runner Telop only
-}
-
-class SpaghettiIds {
-    public static final String INTAKE_MOTOR = "intakeMotor";
-    public static final String LAUNCHER_MOTOR = "launcherMotor";
-    public static final String ARM_SERVO = "armServo";
-    public static final String CLAW_SERVO = "clawServo";
-    public static final String LAUNCHER_SERVO = "launcherServo";
-    public static final String LEFT_INTAKE_SERVO = "leftIntake";
-    public static final String RIGHT_INTAKE_SERVO = "rightIntake";
-    public static final String INTAKE_SERVO = "intakeServo";
-}
-
-class RavioliIds {
-    public static final String MOTOR_CONVEYOR = "conveyor";
-    public static final String MOTOR_LAUNCHER = "launcher";
-    public static final String MOTOR_INTAKE = "intake";
-    public static final String MOTOR_CLAW = "motorClaw";
-    public static final String SERVO_CLAW = "servo_claw";
-    public static final String INTAKE_LOCK = "intakeLock";
 }
