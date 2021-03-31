4f31cee34e11406d0e5636d1a597df7b10ed2e9b
Xevion <xevioni@yandex.com>
2021-03-31 09:58:18 -0500

    MacaroniHardware Claw & Ringstopper Servos, setClaw func
    This is more local VCS changes that never got uploaded properly before the repository overhaul. Basic hardware setup and a setClaw helper function.

--- Team16911/src/main/java/org/firstinspires/ftc/team16911/hardware/MacaroniHardware.java
+++ Team16911/src/main/java/org/firstinspires/ftc/team16911/hardware/MacaroniHardware.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.team16911.hardware.MacaroniIds;
 import org.firstinspires.ftc.teamcode.hardware.RobotHardware;
 
 import java.util.Arrays;
@@ -13,12 +14,15 @@ import java.util.Arrays;
  * Contains references to HardwareMap and all motors, encoders and sensors used.
  */
 public class MacaroniHardware extends RobotHardware {
-    public DcMotorEx intakeMotor;
-    public DcMotorEx beltMotor;
-    public DcMotorEx launcherMotor;
+    private DcMotorEx intakeMotor;
+    private DcMotorEx beltMotor;
+    private DcMotorEx launcherMotor;
 
-    public Servo clawServo;
+    private ElapsedTime launcherElapsed;
+    private Servo leftClawServo;
+    private Servo rightClawServo;
     public Servo armServo;
+    private Servo ringStopperServo;
 
 
     @Override
@@ -30,16 +34,29 @@ public class MacaroniHardware extends RobotHardware {
         beltMotor = hardwareMap.get(DcMotorEx.class, MacaroniIds.BELT_MOTOR);
 
         armServo = hardwareMap.get(Servo.class, MacaroniIds.ARM_SERVO);
-        // clawServo = hardwareMap.get(Servo.class, MacaroniIds.CLAW_SERVO);
+        leftClawServo = hardwareMap.get(Servo.class, MacaroniIds.LEFT_CLAW_SERVO);
+        rightClawServo = hardwareMap.get(Servo.class, MacaroniIds.RIGHT_CLAW_SERVO);
+        ringStopperServo = hardwareMap.get(Servo.class, MacaroniIds.RING_STOPPER_SERVO);
 
         motors.addAll(Arrays.asList(intakeMotor, launcherMotor, beltMotor));
-        servos.addAll(Arrays.asList(armServo, clawServo));
+        servos.addAll(Arrays.asList(armServo, leftClawServo, rightClawServo, ringStopperServo));
 
         motorRightFront.setDirection(DcMotorEx.Direction.REVERSE);
         motorRightRear.setDirection(DcMotorEx.Direction.REVERSE);
+
         beltMotor.setDirection(DcMotorEx.Direction.REVERSE);
         intakeMotor.setDirection(DcMotorEx.Direction.REVERSE);
 
         initializeComponents();
+
+
+        armServo.setPosition(1);
+        setClaws(0);
+        ringStopperServo.setPosition(1);
+    }
+
+    public void setClaws(double position) {
+        leftClawServo.setPosition(1 - position);
+        rightClawServo.setPosition(position);
     }
 }
