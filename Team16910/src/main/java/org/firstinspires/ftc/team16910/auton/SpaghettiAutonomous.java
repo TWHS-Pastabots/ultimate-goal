42d79bb021de3f47d98fe79f7a3995777e970d56
Matt Provost <mattprovost6@gmail.com>
2021-04-17 11:47:59 -0500

    Add lots of comments to Spaghetti code

--- Team16910/src/main/java/org/firstinspires/ftc/team16910/auton/SpaghettiAutonomous.java
+++ Team16910/src/main/java/org/firstinspires/ftc/team16910/auton/SpaghettiAutonomous.java
@@ -23,6 +23,7 @@ import java.util.List;
 @Config
 @Autonomous(name = "Spaghetti Autonomous", preselectTeleOp = "Spaghetti")
 public class SpaghettiAutonomous extends LinearOpMode {
+    // Vuforia/TFOD related variables
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String QUAD_LABEL = "Quad";
     private static final String SINGLE_LABEL = "Single";
@@ -30,16 +31,20 @@ public class SpaghettiAutonomous extends LinearOpMode {
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
 
+    // Useful symbolic on and off constants
     public static double ON = 1;
     public static double OFF = 0;
 
+    // Timings for arm and claw actions
     public static double ARM_TIME = 600;
     public static double CLAW_TIME = 400;
 
+    // Launcher-related constants
     public static double LAUNCHER_POWER = 0.56;
     public static double LAUNCHER_SPINUP = 1000;
     public static double LAUNCHER_LAUNCH = 600;
 
+    // Wobble goal related variables
     public static Pose2d WOBBLE_GOAL_1 = new Pose2d(-2.5300820012016763, 52.1048583194061, 0);
     public static Pose2d WOBBLE_GOAL_2 = new Pose2d(20.479536027344817, 28.077649944945843, 0);
     public static double WOBBLE_GOAL_2_TANGENT = Math.toRadians(-90);
@@ -47,15 +52,21 @@ public class SpaghettiAutonomous extends LinearOpMode {
     private Pose2d wobbleGoalPosition;
     private double wobbleGoalTangent = 0;
 
+    // The final position of the robot. This is an estimate to begin with,
+    // but is set at the end of the opmode so that it can be used elsewhere,
+    // i.e. the Spaghetti teleop
     public static Pose2d FINAL_POSITION = new Pose2d(12, 18, 0);
 
+    // Hardware-related variables
     private SpaghettiHardware robot;
     private SampleMecanumDrive drive;
 
+    // Trajectories
     private Trajectory toWobbleGoal, toPowerShot1, toPowerShot1_2, toPowerShot2, toPowerShot3, toFinish;
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // Don't automatically clear the telemetry
         telemetry.setAutoClear(false);
 
         // Initialize the robot hardware
@@ -66,6 +77,7 @@ public class SpaghettiAutonomous extends LinearOpMode {
         drive = new SampleMecanumDrive(hardwareMap);
         drive.setPoseEstimate(new Pose2d(-62.358, 48.24, 0));
 
+        // Activate TFOD
         activateTfod();
 
         // Initialize the arm
@@ -77,17 +89,20 @@ public class SpaghettiAutonomous extends LinearOpMode {
             return;
         }
 
+        // Detect rings using TFOD
         detectRings();
 
         // Prepare the trajectories needed throughout the opmode
         prepareTrajectories();
 
+        // Update telemetry to display TFOD info, if possible
         telemetry.update();
 
         // Move to the wobble goal and drop it
         drive.followTrajectory(toWobbleGoal);
         dropWobbleGoal();
 
+        // Update telemetry to display TFOD info, if possible
         telemetry.update();
 
         // Move to the powershot goal and shoot the rings
@@ -102,6 +117,8 @@ public class SpaghettiAutonomous extends LinearOpMode {
      *
      */
     private void prepareTrajectories() {
+        // Get the wobble goal position to drive to, if TFOD was able to detect
+        // one, or use the default position
         Pose2d wobbleGoal = wobbleGoalPosition;
         if (wobbleGoal == null) {
             wobbleGoal = WOBBLE_GOAL_1;
@@ -136,14 +153,19 @@ public class SpaghettiAutonomous extends LinearOpMode {
      *
      */
     private void dropWobbleGoal() {
+        // Drop the arm servo and sleep so that it has time to get to that position
         robot.armServo.setPosition(OFF);
         sleep((long) ARM_TIME);
 
+        // Open the claw servo and sleep so that it has time to get to that position
         robot.clawServo.setPosition(OFF);
         sleep((long) CLAW_TIME);
 
+        // Drive backwards so that we don't have any issues with
+        // the wobble goal and raising the arm
         drive.followTrajectory(toPowerShot1);
 
+        // Raise the arm
         robot.armServo.setPosition(ON);
         sleep((long) ARM_TIME);
     }
@@ -152,15 +174,18 @@ public class SpaghettiAutonomous extends LinearOpMode {
      *
      */
     private void launchPowerShots() {
+        // Spin up the launcher motor and pause until it can spin to full speed
         robot.launcherMotor.setVelocity(Spaghetti.motorPower(LAUNCHER_POWER));
         sleep((long) LAUNCHER_SPINUP);
 
+        // Launch the rings and move to the correct positions
         launch();
         drive.followTrajectory(toPowerShot2);
         launch();
         drive.followTrajectory(toPowerShot3);
         launch();
 
+        // Turn the launcher motor off and pause until it is off
         robot.launcherMotor.setPower(OFF);
         sleep((long) LAUNCHER_SPINUP);
     }
@@ -169,8 +194,11 @@ public class SpaghettiAutonomous extends LinearOpMode {
      *
      */
     private void launch() {
+        // Turn the launcher servo on and pause until it extends all the way
         robot.launcherServo.setPosition(ON);
         sleep((long) LAUNCHER_LAUNCH);
+
+        // Turn the launcher servo off and pause until it has retracted all the way
         robot.launcherServo.setPosition(OFF);
         sleep((long) LAUNCHER_LAUNCH);
     }
@@ -179,10 +207,13 @@ public class SpaghettiAutonomous extends LinearOpMode {
      *
      */
     private void finish() {
+        // Move to the final position
         drive.followTrajectory(toFinish);
 
+        // Set the final position constant so it can be used in other opmodes
         FINAL_POSITION = drive.getPoseEstimate();
 
+        // Idle until the opmode should stop
         while (!isStopRequested()) {
             idle();
         }
@@ -212,41 +243,66 @@ public class SpaghettiAutonomous extends LinearOpMode {
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, QUAD_LABEL, SINGLE_LABEL);
     }
 
+    /**
+     *
+     */
     private void activateTfod() {
+        // Initialize Vuforia and TFOD
         initVuforia();
         initTfod();
+
+        // Activate TFOD if it can be activated
         if (tfod != null) {
             tfod.activate();
         }
     }
 
+    /**
+     *
+     */
     @SuppressLint("DefaultLocale")
     private void detectRings() {
         if (tfod != null) {
             ElapsedTime time = new ElapsedTime();
             String label = null;
 
+            // Loop while the opmode is active but only for a maximum of 5 seconds
             while (opModeIsActive() && time.seconds() < 5) {
+                // Get a list of recognitions from TFOD
                 List<Recognition> updatedRecognitions = tfod.getRecognitions();
+
+                // Make sure we are able to get recognitions
                 if (updatedRecognitions != null) {
+                    // Clear telemetry and add a bit of data
                     telemetry.clear();
                     telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                    // Check if we only have one recognition. This is to ensure that we
+                    // don't mistake a false-positive recognition with the actual rings
                     if (updatedRecognitions.size() == 1) {
+                        // Get the recognition and remember its label
                         Recognition recognition = updatedRecognitions.get(0);
                         label = recognition.getLabel();
 
+                        // Break out of the loop because we've found the rings
                         break;
                     } else {
+                        // Log how many recognitions we've found (useful for debugging)
                         telemetry.addLine(String.format("Found %d recognitions", updatedRecognitions.size()));
                         telemetry.update();
                     }
                 }
             }
 
+            // Check the label and store the position we need to drive to
             if (label == null) {
                 wobbleGoalPosition = WOBBLE_GOAL_1;
             } else if (label.equals(SINGLE_LABEL)) {
                 wobbleGoalPosition = WOBBLE_GOAL_2;
+
+                // Store the tangent for this position, because if we don't, the robot might drive
+                // on top of the ring, which will mess up the encoders and put it in the wrong
+                // position. This actually happened to us in a match :(
                 wobbleGoalTangent = WOBBLE_GOAL_2_TANGENT;
             } else if (label.equals(QUAD_LABEL)) {
                 wobbleGoalPosition = WOBBLE_GOAL_3;
@@ -254,6 +310,7 @@ public class SpaghettiAutonomous extends LinearOpMode {
                 wobbleGoalPosition = WOBBLE_GOAL_1;
             }
 
+            // Display some more telemetry information, which is, again, useful for debugging
             telemetry.addData("tensorflow time", time.seconds());
             telemetry.addData("recognition label", label);
             telemetry.addLine(String.format("moving to goal at (%.1f, %.1f)", wobbleGoalPosition.getX(), wobbleGoalPosition.getY()));
--- Team16910/src/main/java/org/firstinspires/ftc/team16910/telop/Spaghetti.java
+++ Team16910/src/main/java/org/firstinspires/ftc/team16910/telop/Spaghetti.java
@@ -13,17 +13,23 @@ import org.firstinspires.ftc.team16910.hardware.SpaghettiHardware;
 @TeleOp(name = "Spaghetti", group = "Linear OpMode")
 @Config
 public class Spaghetti extends OpMode {
+    // The small amount to move the robot for fine-tuned movements
     public static double NUDGE_AMOUNT = 0.25;
 
+    // Useful symbolic on and off constants
     public static double ON = 1;
     public static double OFF = 0;
 
+    // Constants related to the intake
     public static double INTAKE_ON = 0.15;
     public static double INTAKE_GEAR_RATIO = 5;
 
+    // Variables used for the launcher
     private double lastLaunchTime = 0;
     public static double LAUNCH_TIME = 0.6;
     public static double LAUNCHER_POWER = 0.6375;
+
+    // Constants related to the motors we're using
     public static double MOTOR_MAX_RPM = 6000;
     public static double MOTOR_TICKS_PER_SECOND = 28;
 
@@ -32,6 +38,11 @@ public class Spaghetti extends OpMode {
      * and manipulates its numeric value to convert it into a velocity that can be used by
      * {@link DcMotorEx#setVelocity(double)}.
      *
+     * What this actually does is takes the maximum motor RPM from {@link #MOTOR_MAX_RPM} and converts it to
+     * the max rotations per second by dividing it by {@code 60}. It then multiplies it by the power level specified
+     * to get the target number of rotations per second and then multiplies it by {@link #MOTOR_TICKS_PER_SECOND}
+     * to convert it to the number of ticks per second used by the motor.
+     *
      * @param power the power level as a decimal
      * @return motor ticks per second
      */
@@ -39,55 +50,73 @@ public class Spaghetti extends OpMode {
         return MOTOR_MAX_RPM / 60 * power * MOTOR_TICKS_PER_SECOND;
     }
 
+    // Constants that scale the input power so movement isn't super jerky
     public static double X_SCALE = 0.7;
     public static double Y_SCALE = 0.7;
     public static double TURN_SCALE = 0.85;
 
+    // A position that is useful for launching rings from
+    // TODO: this should be deleted because it's super not recommended
     public static Pose2d middlePosition = new Pose2d(-22.570475172975446, 53.49077586858332, Math.toRadians(-0.862970095));
 
-    /* Declare OpMode members. */
+    // Hardware-related variables
     private final SpaghettiHardware robot = new SpaghettiHardware();
     private SampleMecanumDrive drive;
 
+    // State tracking variables for the intake
     private boolean previousIntakeButton = false;
     private boolean intakeState = true;
 
+    // State tracking variables for the claw
     private boolean previousClawButton = false;
     private boolean clawState = false;
 
+    // State tracking variables for the arm
     private boolean previousArmButton = false;
     private boolean armState = false;
 
     @Override
     public void init() {
+        // Initialize the robot hardware
         robot.init(hardwareMap);
 
+        // Initialize the drive train
         drive = new SampleMecanumDrive(hardwareMap);
         drive.setPoseEstimate(SpaghettiAutonomous.FINAL_POSITION);
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Status", "Initialized");    //
+        telemetry.addData("Status", "Initialized");
     }
 
     @Override
     public void start() {
-        telemetry.addData("Run Time", "reset");
     }
 
     @Override
     public void loop() {
+        // Separate the movement and operation code into two separate functions
+        // so that it is easier to understand the separation
         movement();
         operation();
 
+        // Update the telemetry
         telemetry.update();
     }
 
+    /**
+     *
+     */
     private void movement() {
+        // Calculate nudge amounts for all of the axis
         double x_nudge = gamepad1.dpad_right ? NUDGE_AMOUNT : (gamepad1.dpad_left ? -NUDGE_AMOUNT : 0);
         double y_nudge = gamepad1.dpad_up ? -NUDGE_AMOUNT : (gamepad1.dpad_down ? NUDGE_AMOUNT : 0);
         double ang_nudge = gamepad1.right_bumper ? NUDGE_AMOUNT : (gamepad1.left_bumper ? -NUDGE_AMOUNT : 0);
 
+        // Check if the drive train is busy, i.e. running a trajectory
         if (!drive.isBusy()) {
+            // Set the drive train power using the weighted method so that
+            // if the numbers end up too large, they are scaled to actually
+            // work well with the drive train
             drive.setWeightedDrivePower(
                     new Pose2d(
                             -(gamepad1.left_stick_y * Y_SCALE + y_nudge),
@@ -102,6 +131,7 @@ public class Spaghetti extends OpMode {
                 // NOTE: apparently this is like super not recommended, but I guess
                 // we can leave it because it isn't actually required, just something
                 // that is there and can be used if needed
+                // TODO: remove this
                 drive.followTrajectoryAsync(drive.trajectoryBuilder(drive.getPoseEstimate())
                         .splineToLinearHeading(middlePosition, Math.toRadians(90))
                         .build());
@@ -110,42 +140,69 @@ public class Spaghetti extends OpMode {
             }
         }
 
+        // Try the update to the drive train. This is surrounded by a try
+        // block in case anything goes wrong in RoadRunner and it, for
+        // whatever reason, throws an error. This makes sure the teleop
+        // and/or phone doesn't crash, which actually happened to us
+        // in a match :(
         try {
+            // Update the drive train to actually set the motor powers
             drive.update();
         } catch (Exception e) {
             telemetry.addData("Error trying to update drive", e);
         }
 
+        // Write some telemetry, which is sometimes useful for debugging
         Pose2d poseEstimate = drive.getPoseEstimate();
         telemetry.addData("x", poseEstimate.getX());
         telemetry.addData("y", poseEstimate.getY());
         telemetry.addData("heading", poseEstimate.getHeading());
     }
 
+    /**
+     *
+     */
     private void operation() {
+        // Set the motor velocity for the launcher motor
         motorVelo(robot.launcherMotor, "launcher motor", motorPower(LAUNCHER_POWER) * gamepad2.right_trigger);
 
+        // Check if the launcher button is pressed, and the launcher should run
         if (gamepad2.left_bumper && lastLaunchTime + LAUNCH_TIME * 2 < getRuntime()) {
+            // Run the launcher and store the time that the launcher is run so that we can
+            // know when to stop it and start it again
             robot.launcherServo.setPosition(ON);
             lastLaunchTime = getRuntime();
         }
 
+        // Check if it is time to stop the launcher
         if (lastLaunchTime + LAUNCH_TIME < getRuntime()) {
+            // Stop the launcher
             robot.launcherServo.setPosition(OFF);
         }
 
+        // Check if the intake should be turned on
         if (gamepad2.right_bumper) {
+            // Turn on the intake motor using velocity, and turn on the intake servo
             motorVelo(robot.intakeMotor, "intake motor", motorPower(INTAKE_ON) * INTAKE_GEAR_RATIO);
             robot.intakeServo.setPower(ON);
         } else {
+            // Turn off the intake motor, and turn off the intake servo
             motorVelo(robot.intakeMotor, "intake motor", OFF);
             robot.intakeServo.setPower(OFF);
         }
 
+        // Check if the claw button is being pressed and see if that state is different from the
+        // state it was in the last loop. This is essentially just checking if there was a change
+        // in state from the last loop to this one
         boolean clawButton = gamepad2.y;
         if (clawButton != previousClawButton) {
+            // Check if the claw button is actually down. This makes sure our state is changed
+            // only when the button is just pressed, and no other time
             if (clawButton) {
+                // Invert the claw state
                 clawState = !clawState;
+
+                // Update the claw servo to reflect the new claw state
                 if (clawState) {
                     robot.clawServo.setPosition(ON);
                 } else {
@@ -153,13 +210,22 @@ public class Spaghetti extends OpMode {
                 }
             }
 
+            // Store the current claw button state as the previous one to be use for the next loop
             previousClawButton = clawButton;
         }
 
+        // Check if the arm button is being pressed and see if that state is different from the
+        // state it was in the last loop. This is essentially just checking if there was a change
+        // in state from the last loop to this one
         boolean armButton = gamepad2.b;
         if (armButton != previousArmButton) {
+            // Check if the arm button is actually down. This makes sure our state is changed
+            // only when the button is just pressed, and no other time
             if (armButton) {
+                // Invert the arm state
                 armState = !armState;
+
+                // Update the arm servo to reflect the new arm state
                 if (armState) {
                     robot.armServo.setPosition(OFF);
                 } else {
@@ -167,13 +233,22 @@ public class Spaghetti extends OpMode {
                 }
             }
 
+            // Store the current arm button state as the previous one to be use for the next loop
             previousArmButton = armButton;
         }
 
+        // Check if the intake button is being pressed and see if that state is different from the
+        // state it was in the last loop. This is essentially just checking if there was a change
+        // in state from the last loop to this one
         boolean intakeButton = gamepad2.a;
         if (intakeButton != previousIntakeButton) {
+            // Check if the intake button is actually down. This makes sure our state is changed
+            // only when the button is just pressed, and no other time
             if (intakeButton) {
+                // Invert the intake state
                 intakeState = !intakeState;
+
+                // Update the intake servos to reflect the new intake state
                 if (intakeState) {
                     robot.leftIntakeServo.setPosition(ON);
                     robot.rightIntakeServo.setPosition(ON);
@@ -183,17 +258,27 @@ public class Spaghetti extends OpMode {
                 }
             }
 
+            // Store the current intake button state as the previous one to be use for the next loop
             previousIntakeButton = intakeButton;
         }
     }
 
+    /**
+     *
+     * @param motor
+     * @param name
+     * @param amount
+     */
     private void motorVelo(DcMotorEx motor, String name, double amount) {
+        // Set the motor's velocity to the specified amount
         motor.setVelocity(amount);
 
+        // Get the current and target velocity that the motor should be at and calculate the percent error
         double target = amount;
         double actual = motor.getVelocity();
         double error = Math.abs(target - actual) / target;
 
+        // Log all of the information to telemetry. This is often very useful for debugging
         telemetry.addData(String.format("%s target velocity", name), target);
         telemetry.addData(String.format("%s actual velocity", name), actual);
         telemetry.addData(String.format("%s velocity error", name), error);
