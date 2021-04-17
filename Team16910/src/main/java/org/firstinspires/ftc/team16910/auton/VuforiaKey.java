550d7e04a577c016393f5bf0afcd30ef62056657
Matt Provost <mattprovost6@gmail.com>
2021-04-17 10:59:49 -0500

    Add Vuforia key

---
+++ Team16910/src/main/java/org/firstinspires/ftc/team16910/auton/VuforiaKey.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.team16910.auton;
+
+public class VuforiaKey {
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    public static final String KEY = " --- YOUR NEW VUFORIA KEY GOES HERE  --- ";
+}
