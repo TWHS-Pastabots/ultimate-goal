8a76e22149e57155a0692abf6345e637f3461155
Matt Provost <mattprovost6@gmail.com>
2021-05-19 07:52:59 -0500

    Add arm position detector teleop

---
+++ Team15021/src/main/java/org/firstinspires/ftc/team15021/telop/ArmPositionDetector.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.team15021.telop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.team15021.hardware.RavioliHardware;
+
+@TeleOp(name = "Arm Position Detector")
+public class ArmPositionDetector extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        RavioliHardware robot = new RavioliHardware();
+        robot.init(hardwareMap);
+
+        waitForStart();
+        if (!opModeIsActive()) {
+            return;
+        }
+
+        while (!isStopRequested()) {
+            telemetry.addData("Arm position", "%.02f", robot.motorClaw.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
